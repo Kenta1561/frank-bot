9e26b2cb1baa71594072ad8b43fc785aa09b60f9
Kenta Kariya <kentakariya@outlook.com>
2021-01-24 21:15:23 +0100

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.koin:koin-core:2.2.2")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.discord4j:discord4j-core:3.1.3")
     testImplementation(kotlin("test-junit"))
 }
 
